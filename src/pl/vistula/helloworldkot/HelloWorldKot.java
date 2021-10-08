bb869b9dcedbe31b5dc7f9743191cee5b0903eeb
student <student@lab.pl.wsei>
2021-10-08 11:46:11 +0200

    update

--- src/pl/vistula/helloworldkot/HelloWorldKot.java
+++ src/pl/vistula/helloworldkot/HelloWorldKot.java
@@ -3,5 +3,6 @@ package pl.vistula.helloworldkot;
 public class HelloWorldKot {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Kot");
+        System.out.println("I'm learning Java now!");
     }
 }
