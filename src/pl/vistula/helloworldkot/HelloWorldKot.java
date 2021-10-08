8ee18bdd5691f54d560d5e27457a0d6481b12fde
student <student@lab.pl.wsei>
2021-10-08 11:59:57 +0200

    add_variables

--- src/pl/vistula/helloworldkot/HelloWorldKot.java
+++ src/pl/vistula/helloworldkot/HelloWorldKot.java
@@ -4,5 +4,10 @@ public class HelloWorldKot {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Kot");
         System.out.println("I'm learning Java now!");
+
+        int x = 0;
+        int y = 10;
+        int z = x + y;
+        System.out.println(z);
     }
 }
